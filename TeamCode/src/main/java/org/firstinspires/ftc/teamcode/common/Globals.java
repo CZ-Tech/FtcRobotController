08905c36b3811085b97b0b8b0467ddd3638619bc
Shawphy <shawphy@gmail.com>
2024-11-15 01:38:50 +0800

    Alpha test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Globals.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.common;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+
+public class Globals {
+    // Rev HD Hex Motor (No Gearbox) : 28 counts/revolution
+    // eg: GoBILDA 312 RPM (19.2:1) Yellow Jacket 537.7=28*19.2
+    // 40:1 Rev 28*40=1120
+    // 20:1 Rev 28*20=560
+    // Calculate the COUNTS_PER_INCH for your specific drive train.
+    // Go to your motor vendor website to determine your motor's COUNTS_PER_MOTOR_REV
+    // For external drive gearing, set DRIVE_GEAR_REDUCTION as needed.
+    // For example, use a value of 2.0 for a 12-tooth spur gear driving a 24-tooth spur gear.
+    // This is gearing DOWN for less speed and more torque.
+    // For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
+    public static final double COUNTS_PER_MOTOR_REV = 537.7;
+    public static final double DRIVE_GEAR_REDUCTION = 1.0;     // No External Gearing.没有外部齿轮转动
+    public static final double WHEEL_DIAMETER_INCHES = 4.00;     // 96cm For figuring circumference
+    public static double HEADING_THRESHOLD = 1.0;    // 在移动到下一步时头部必须离目标有多近
+
+    // 定义移动常量. 定义public类型以在opmode中调用
+    public static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * Math.PI);
+
+    public static final double P_DRIVE_GAIN = 0.03;     // 更大的数值使它更加灵敏, 但会让它更不稳定
+    public static final double P_STRAFE_GAIN = 0.025;   // 平移速度控制 "Gain".
+    public static final double P_TURN_GAIN = 0.02;     // 更大的数值使它更加灵敏, 但会让它更不稳定
+    public static final double LATERAL_DISTANCE = 10.63;  //TODO:调试此值，让机器走准
+    public static final double FORWARD_OFFSET = 5.12;  //TODO:调试此值，让机器走准
+//  +++++++++++++++++++++++++++++++
+//  +  |||                   |||  +
+//  +  ||| LATERAL_DISTANCE  |||  +
+//  +     <----------------->     +
+//  +             ● Center        +        FIXME:Center指的是机器的旋转中心
+//  +             |               +
+//  +             | FORWARD_OFFSET+
+//  +             V               +
+//  +           =====             +
+//  +           =====             +
+//  +++++++++++++++++++++++++++++++
+
+
+    public static final RevHubOrientationOnRobot orientationOnRobot=new RevHubOrientationOnRobot(
+            RevHubOrientationOnRobot.LogoFacingDirection.UP,
+            RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+    );
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Robot.java
@@ -0,0 +1,81 @@
+package org.firstinspires.ftc.teamcode.common;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.common.drive.Drivetrain;
+import org.firstinspires.ftc.teamcode.common.hardware.GamepadEx;
+import org.firstinspires.ftc.teamcode.common.hardware.Odometry;
+import org.firstinspires.ftc.teamcode.common.subsystem.Subsystem;
+import org.firstinspires.ftc.teamcode.common.vision.Vision;
+
+public enum Robot {
+    INSTANCE;
+    public HardwareMap hardwareMap;
+    public Telemetry telemetry;
+    public Vision vision;
+    public LinearOpMode opMode;
+    public Drivetrain drivetrain;
+    public Subsystem subsystem;
+    public GamepadEx gamepad1;
+    public GamepadEx gamepad2;
+    public Odometry odometry;
+    public IMU imu;
+
+    /**
+     * 初始化机器人
+     *
+     * @param opMode 在 opMode 里通过 robot.init(this); 进行初始化。
+     */
+    public void init(LinearOpMode opMode) {
+        this.opMode = opMode;
+        this.hardwareMap = opMode.hardwareMap; //硬件映射
+        this.telemetry = opMode.telemetry; //DH上的遥测
+        this.vision = Vision.INSTANCE; //视觉模块
+        this.drivetrain = Drivetrain.INSTANCE; //机器人控制函数
+        this.subsystem = Subsystem.INSTANCE; //上层子系统
+        this.odometry = Odometry.INSTANCE; //里程计
+        this.gamepad1 = new GamepadEx(opMode.gamepad1); //一个控制器
+        this.gamepad2 = new GamepadEx(opMode.gamepad2); //另一个控制器
+
+
+        //获取并初始化陀螺仪
+        imu = hardwareMap.get(IMU.class, "imu"); //获取陀螺仪
+        imu.initialize(new IMU.Parameters(Globals.orientationOnRobot)); //初始化陀螺仪
+    }
+
+    /**
+     * 获取当前电压
+     *
+     * @return voltage
+     */
+    public double getVoltage() {
+        return hardwareMap.voltageSensor.iterator().next().getVoltage();
+    }
+
+    /**
+     * 以多线程运行函数
+     *
+     * @param commands 需要同时运行的命令（函数或者方法）
+     * @return Robot
+     */
+    public Robot syncRun(Runnable... commands) {
+        for (Runnable command : commands) {
+            new Thread(command).start();
+        }
+        return this;
+    }
+
+    /**
+     * 停止机器人的动作，时间以毫秒为单位
+     *
+     * @param milliseconds 需要暂停的毫秒数量
+     * @return Robot
+     */
+    public Robot sleep(long milliseconds) {
+        opMode.sleep(milliseconds);
+        return this;
+    }
+}
